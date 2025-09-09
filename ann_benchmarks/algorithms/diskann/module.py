7b557a5e5d9280d0bda58b946453713ba9bf498e
Theodor Wübker <theo@jmop.de>
2025-09-09 13:32:58 +0200

    DiskANN: Modernize module
    Currently, an old implementation of the diskann python bindings is used.
    Microsoft has an updated implementation called diskannpy available now.
    
    Replace the old diskann module with a new version that uses the
    StaticMemoryIndex from diskannpy. Add corresponding config and dockerfile. Also
    reduce the complexity of the previous code by removing unnecessary functions,
    leading to a minimal version comparable to the hnswlib module.

--- ann_benchmarks/algorithms/diskann/module.py
+++ ann_benchmarks/algorithms/diskann/module.py
@@ -1,191 +1,52 @@
-import os
-import struct
-import time
-
-import numpy as np
-import vamanapy as vp
+import diskannpy
+import tempfile
 
 from ..base.module import BaseANN
 
 
-class Vamana(BaseANN):
-    def __init__(self, metric, param):
+class DiskANN(BaseANN):
+    def __init__(self, metric, method_param):
+        self.name = None
+        self.search_complexity = None
         self.metric = {"angular": "cosine", "euclidean": "l2"}[metric]
-        self.l_build = int(param["l_build"])
-        self.max_outdegree = int(param["max_outdegree"])
-        self.alpha = float(param["alpha"])
-        print("Vamana: L_Build = " + str(self.l_build))
-        print("Vamana: R = " + str(self.max_outdegree))
-        print("Vamana: Alpha = " + str(self.alpha))
-        self.params = vp.Parameters()
-        self.params.set("L", self.l_build)
-        self.params.set("R", self.max_outdegree)
-        self.params.set("C", 750)
-        self.params.set("alpha", self.alpha)
-        self.params.set("saturate_graph", False)
-        self.params.set("num_threads", 1)
+        self.method_param = method_param
+        self.index = None
+        self.tempdir = tempfile.TemporaryDirectory()
 
     def fit(self, X):
-        def bin_to_float(binary):
-            return struct.unpack("!f", struct.pack("!I", int(binary, 2)))[0]
+        max_degree = self.method_param.get("max_degree", 64)
+        complexity = self.method_param.get("complexity", 128)
+        alpha = self.method_param.get("alpha", 1.2)
 
-        print("Vamana: Starting Fit...")
-        index_dir = "indices"
+        diskannpy.build_memory_index(
+            data=X,
+            distance_metric=self.metric,
+            index_directory=self.tempdir.name,
+            complexity=complexity, # candidate NN list when building. Typically 75-200. At least as large as graph degree.
+            graph_degree=max_degree, # max graph degree. Typically 60-150. Higher means better recall.
+            num_threads=0, # uses max available processors
+            alpha=alpha, # controls number of points added to the graph
+            use_pq_build=False, # uses quantization to save index which reduces recall and disk space
+            use_opq=False,
+        )
 
-        if not os.path.exists(index_dir):
-            os.makedirs(index_dir)
+        self.index = diskannpy.StaticMemoryIndex(
+            index_directory=self.tempdir.name,
+            num_threads=1,
+            initial_search_complexity=100, # most common complexity durig search. Working mem is initialized based off this * threads.
+            distance_metric=self.metric,
+            dimensions = None
+        )
 
-        data_path = os.path.join(index_dir, "base.bin")
-        self.name = "Vamana-{}-{}-{}".format(self.l_build, self.max_outdegree, self.alpha)
-        save_path = os.path.join(index_dir, self.name)
-        print("Vamana: Index Stored At: " + save_path)
-        shape = [
-            np.float32(bin_to_float("{:032b}".format(X.shape[0]))),
-            np.float32(bin_to_float("{:032b}".format(X.shape[1]))),
-        ]
-        X = X.flatten()
-        X = np.insert(X, 0, shape)
-        X.tofile(data_path)
-
-        if not os.path.exists(save_path):
-            print("Vamana: Creating Index")
-            s = time.time()
-            if self.metric == "l2":
-                index = vp.SinglePrecisionIndex(vp.Metric.FAST_L2, data_path)
-            elif self.metric == "cosine":
-                index = vp.SinglePrecisionIndex(vp.Metric.INNER_PRODUCT, data_path)
-            else:
-                print("Vamana: Unknown Metric Error!")
-            index.build(self.params, [])
-            t = time.time()
-            print("Vamana: Index Build Time (sec) = " + str(t - s))
-            index.save(save_path)
-        if os.path.exists(save_path):
-            print("Vamana: Loading Index: " + str(save_path))
-            s = time.time()
-            if self.metric == "l2":
-                self.index = vp.SinglePrecisionIndex(vp.Metric.FAST_L2, data_path)
-            elif self.metric == "cosine":
-                self.index = vp.SinglePrecisionIndex(vp.Metric.INNER_PRODUCT, data_path)
-            else:
-                print("Vamana: Unknown Metric Error!")
-            self.index.load(file_name=save_path)
-            print("Vamana: Index Loaded")
-            self.index.optimize_graph()
-            print("Vamana: Graph Optimization Completed")
-            t = time.time()
-            print("Vamana: Index Load Time (sec) = " + str(t - s))
-        else:
-            print("Vamana: Unexpected Index Build Time Error")
-
-        print("Vamana: End of Fit")
-
-    def set_query_arguments(self, l_search):
-        print("Vamana: L_Search = " + str(l_search))
-        self.l_search = l_search
+    def set_query_arguments(self, complexity):
+        self.search_complexity = complexity
+        self.name = "diskann (%s, 'complexity': %s)" % (self.method_param, complexity)
 
     def query(self, v, n):
-        return self.index.single_numpy_query(v, n, self.l_search)
-
-    def batch_query(self, X, n):
-        self.num_queries = X.shape[0]
-        self.result = self.index.batch_numpy_query(X, n, self.num_queries, self.l_search)
-
-    def get_batch_results(self):
-        return self.result.reshape((self.num_queries, self.result.shape[0] // self.num_queries))
+        ids, distances = self.index.search(v, n, self.search_complexity)
+        return ids
 
 
-class VamanaPQ(BaseANN):
-    def __init__(self, metric, param):
-        self.metric = {"angular": "cosine", "euclidean": "l2"}[metric]
-        self.l_build = int(param["l_build"])
-        self.max_outdegree = int(param["max_outdegree"])
-        self.alpha = float(param["alpha"])
-        self.chunks = int(param["chunks"])
-        print("Vamana PQ: L_Build = " + str(self.l_build))
-        print("Vamana PQ: R = " + str(self.max_outdegree))
-        print("Vamana PQ: Alpha = " + str(self.alpha))
-        print("Vamana PQ: Chunks = " + str(self.chunks))
-        self.params = vp.Parameters()
-        self.params.set("L", self.l_build)
-        self.params.set("R", self.max_outdegree)
-        self.params.set("C", 750)
-        self.params.set("alpha", self.alpha)
-        self.params.set("saturate_graph", False)
-        self.params.set("num_chunks", self.chunks)
-        self.params.set("num_threads", 1)
-
-    def fit(self, X):
-        def bin_to_float(binary):
-            return struct.unpack("!f", struct.pack("!I", int(binary, 2)))[0]
-
-        print("Vamana PQ: Starting Fit...")
-        index_dir = "indices"
-
-        if self.chunks > X.shape[1]:
-            raise ValueError
-
-        if not os.path.exists(index_dir):
-            os.makedirs(index_dir)
-
-        data_path = os.path.join(index_dir, "base.bin")
-        pq_path = os.path.join(index_dir, "pq_memory_index")
-        self.name = "VamanaPQ-{}-{}-{}".format(self.l_build, self.max_outdegree, self.alpha)
-        save_path = os.path.join(index_dir, self.name)
-        print("Vamana PQ: Index Stored At: " + save_path)
-        shape = [
-            np.float32(bin_to_float("{:032b}".format(X.shape[0]))),
-            np.float32(bin_to_float("{:032b}".format(X.shape[1]))),
-        ]
-        X = X.flatten()
-        X = np.insert(X, 0, shape)
-        X.tofile(data_path)
-
-        if not os.path.exists(save_path):
-            print("Vamana PQ: Creating Index")
-            s = time.time()
-            if self.metric == "l2":
-                index = vp.SinglePrecisionIndex(vp.Metric.FAST_L2, data_path)
-            elif self.metric == "cosine":
-                index = vp.SinglePrecisionIndex(vp.Metric.INNER_PRODUCT, data_path)
-            else:
-                print("Vamana PQ: Unknown Metric Error!")
-            index.pq_build(data_path, pq_path, self.params)
-            t = time.time()
-            print("Vamana PQ: Index Build Time (sec) = " + str(t - s))
-            index.save(save_path)
-        if os.path.exists(save_path):
-            print("Vamana PQ: Loading Index: " + str(save_path))
-            s = time.time()
-            if self.metric == "l2":
-                self.index = vp.SinglePrecisionIndex(vp.Metric.FAST_L2, data_path)
-            elif self.metric == "cosine":
-                self.index = vp.SinglePrecisionIndex(vp.Metric.INNER_PRODUCT, data_path)
-            else:
-                print("Vamana PQ: Unknown Metric Error!")
-            self.index.load(file_name=save_path)
-            print("Vamana PQ: Index Loaded")
-            self.index.pq_load(pq_prefix_path=pq_path)
-            print("Vamana PQ: PQ Data Loaded")
-            self.index.optimize_graph()
-            print("Vamana PQ: Graph Optimization Completed")
-            t = time.time()
-            print("Vamana PQ: Index Load Time (sec) = " + str(t - s))
-        else:
-            print("Vamana PQ: Unexpected Index Build Time Error")
-
-        print("Vamana PQ: End of Fit")
-
-    def set_query_arguments(self, l_search):
-        print("Vamana PQ: L_Search = " + str(l_search))
-        self.l_search = l_search
-
-    def query(self, v, n):
-        return self.index.pq_single_numpy_query(v, n, self.l_search)
-
-    def batch_query(self, X, n):
-        self.num_queries = X.shape[0]
-        self.result = self.index.pq_batch_numpy_query(X, n, self.num_queries, self.l_search)
-
-    def get_batch_results(self):
-        return self.result.reshape((self.num_queries, self.result.shape[0] // self.num_queries))
+    def done(self):
+        self.tempdir.cleanup()
+        pass
